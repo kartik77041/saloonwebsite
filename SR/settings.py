cb2b9a6107192722438c5e63593a1a4490deb0e1
kartik <Kartikrajput77041@gmail.com>
2023-11-17 20:51:58 +0530

    again app

--- SR/settings.py
+++ SR/settings.py
@@ -111,9 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'app/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "app/static/"),
-)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
